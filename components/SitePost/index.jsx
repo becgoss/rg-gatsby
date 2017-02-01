1d3ea0912e650304ce14a45bf00cf0e0295e6028
Jonathan Heslop <jonathan.heslop@digitaslbi.com>
2017-02-01 10:51:19 +0000

    Homepage and post page take metadata dynamically

--- components/SitePost/index.jsx
+++ components/SitePost/index.jsx
@@ -5,13 +5,11 @@ import { prefixLink } from 'gatsby-helpers'
 import access from 'safe-access'
 import { config } from 'config'
 import SiteHeader from '../SiteHeader'
-import './style.css'
 import '../../static/css/highlight.css'
 import '../../static/css/slick.css'
 import '../../static/css/slick-theme.css'
 import $ from 'jquery';
 import '../../static/js/slick.min.js'
-import NewsflareImage from '../../static/newsflare-phone-header.png'
 
 class SitePost extends React.Component {
     componentDidMount() {
@@ -28,19 +26,20 @@ class SitePost extends React.Component {
     render() {
         const {route} = this.props
         const post = route.page.data
+        const imagePath = post.image
 
         return (
             <div>
               <SiteHeader {...this.props}/>
-              <div className='w-100 vh-75 dt relative overflow-hidden bg-light-gray'>
+              <div className={`w-100 vh-75 dt relative overflow-hidden ${ post.headerBgColor }`}>
                 <div className='dtc v-mid'>
-                  <header className='near-black ml6'>
+                  <header className={`ml6 ${ post.headerTextColor }`}>
                     <h1 className='f-headline lh-title fw6 mv0'>{ post.title }</h1>
-                    <h3 className='f6 fw3 lh-title mt0 i'>Case Study, Spring 2017</h3>
-                    <p className="measure">Newsflare are a company specialising in user generated video which they license to news organisations around the world. Users upload content to the Newsflare platform through desktop or a mobile application.</p>
+                    <h3 className='f6 fw3 lh-title mt0 i'>{ post.category }, { post.season }</h3>
+                    <p className="measure">{ post.description }</p>
                   </header>
                 </div>
-                <img src={NewsflareImage} alt='Newsflare Home Screen' className='absolute top-1 right-2' />
+                <img src={ imagePath } alt={ post.title } className='absolute top-1 right-2' />
               </div>
               <main role='main' className='pa3 ph5-ns'>
                 <div className='lh-copy' dangerouslySetInnerHTML={ {    __html: post.body} } />
--- html.js
+++ html.js
@@ -12,7 +12,6 @@ module.exports = React.createClass({
     render() {
         const {body, route} = this.props
         const {title} = Helmet.rewind()
-        const font = <link href='https://fonts.googleapis.com/css?family=Roboto:400,400italic,500,700&subset=latin,cyrillic' rel='stylesheet' type='text/css' />
         let css
         if (process.env.NODE_ENV === 'production') {
             css = <style dangerouslySetInnerHTML={ {    __html: require('!raw!./public/styles.css')} } />
@@ -25,7 +24,6 @@ module.exports = React.createClass({
               <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
               <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=5.0" />
               { title.toComponent() }
-              { font }
               { css }
             </head>
             <body className="avenir black">
